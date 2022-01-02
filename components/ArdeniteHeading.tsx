dc5a53df2b88c4779e8723aaa265630a27340a15
Beni <thorstensen.benjamin@gmail.com>
2022-01-02 13:40:49 +0100

    ipad: menu alignment & hovering heading alignment

---
+++ components/ArdeniteHeading.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+const ArdeniteHeading = () => {
+
+    return (
+        <div className={" text-2xl md:text-2xl leading-6  text-pink filter drop-shadow-xs font-semibold  whitespace-pre"}>
+            DARK SYNTH {'\n'}POP ROCK {'\n'}DUO FROM {'\n'}VIENNA
+        </div>
+
+
+    );
+}
+
+export default ArdeniteHeading
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,48 +4,51 @@ import ardeniteDuo from "/public/static/images/ardenite-duo.png";
 
 import Header from "../components/Header";
 import ArdeniteOutlineButton from "../components/ArdeniteOutlineButton";
+import ArdeniteHeading from "../components/ArdeniteHeading";
 
 const IndexPage = () => (
     <div className={"flex justify-center min-w-full h-screen min-w-max "}>
         <div className={"flex flex-col px-8 sm:px-16  w-full max-w-screen-xl"}>
             <Header/>
 
-            <div >
+            <div>
 
                 <div className={"flex flex-col sm:flex-row "}
-                    >
-                    <div className={"absolute z-10  text-2xl md:text-2xl leading-6 px-4  "}>
-                        <div className={"text-pink filter drop-shadow-xs font-semibold  whitespace-pre"}>
-                            DARK SYNTH {'\n'}POP ROCK
-                        </div>
-                        <div className={"text-pink filter drop-shadow-xs font-semibold  whitespace-pre"}>
-                            DUO FROM {'\n'}VIENNA
-                        </div>
+                >
+
+
+                    <div className={"absolute z-10 px-4 sm:invisible"}>
+                        <ArdeniteHeading/>
                     </div>
-                    <div className={"pl-16 pt-7 sm:w-1/2 sm:order-2"}>
+                    <div className={"pl-16 pt-7 sm:pl-5 sm:basis-1/2 sm:order-2 flex flex-col-reverse "}>
 
                         <Image
                             src={ardeniteDuo}
                             alt="Ardenite"
                             layout={"responsive"}
                             objectFit={"contain"}
-                            objectPosition={"left"}
                         />
 
                     </div>
 
-                    <div className={"flex flex-col sm:w-1/2 px-8 sm:px-0 sm:self-end sm:px-4"}
-                    >
 
-                        <ArdeniteOutlineButton title={"LISTEN NOW"}/>
-                        <ArdeniteOutlineButton title={"WATCH NOW"}/>
-                        <ArdeniteOutlineButton title={"BUY NOW"}/>
+
+                    <div className={"flex flex-col justify-between sm:basis-2/5 px-8 sm:px-0  sm:px-4 "}
+                    >
+                        <div className={"hidden sm:flex"}>
+                            <ArdeniteHeading/>
+                        </div>
+
+                        <div className={""}>
+                            <ArdeniteOutlineButton title={"LISTEN NOW"}/>
+                            <ArdeniteOutlineButton title={"WATCH NOW"}/>
+                            <ArdeniteOutlineButton title={"BUY NOW"}/>
+                        </div>
 
 
                     </div>
 
                 </div>
-
 
 
             </div>
