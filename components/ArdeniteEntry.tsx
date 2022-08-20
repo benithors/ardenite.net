38df3efb02daf88f8d7ed14adb43921e36d06146
Beni <thorstensen.benjamin@gmail.com>
2022-08-20 21:28:50 +0200

    content section

--- components/ArdeniteEntry.tsx
+++ components/ArdeniteEntry.tsx
@@ -1,29 +1,49 @@
 import React from 'react'
 import {ArrowUpRight} from "react-feather";
 import Image from "next/image";
+import {useInView} from "react-intersection-observer";
+
+export enum EntryPosition {
+    left = 0,
+    right = 1,
+
+}
 
 interface IProps {
-    direction: string;
+    animation: EntryPosition;
     image: any
     subtitle: string;
     title: string;
-    children:any
+    children: any
+    className?: string;
+    priority:boolean
 }
+
 
 const ArdeniteEntry = (props: IProps) => {
 
+
+    const entryInView = useInView({
+        triggerOnce: true,
+        rootMargin: "0px 0px",
+    });
+
+    let animationDir = props.animation === EntryPosition.left ? " translate-y-12 md:translate-x-12 " : " translate-y-12  md:-translate-x-0 ";
+
     return (
-        <div className={'bg-white w-full h-full flex flex-col-reverse md:flex-row'}>
+        <div ref={entryInView.ref} className={props.className + (" overflow-x-hidden  w-full h-full flex flex-col-reverse  ") + (props.animation === EntryPosition.left ? " md:flex-row " : "  md:flex-row-reverse ")}>
 
-            <div className={'text-white bg-teal-900 w-full md:w-1/2 flex flex-col  p-12 '}>
 
-                <div className={' flex flex-col  justify-center items-center md:items-start space-y-8'}>
+            <div className={'text-white  w-full md:w-1/2  flex flex-col  p-12 '}>
+
+                <div className={' flex flex-col w-full md:w-[calc(100vw/3)]  justify-center transition-all duration-1000  items-center md:items-start space-y-8 ' + (entryInView.inView ? " opacity-100 translate-x-0 " +
+                    " translate-y-0 " : " opacity-0 " + animationDir)}>
 
                     <h2 className={'opacity-70'}>
                         {props.subtitle}
                     </h2>
 
-                    <h1 className={'text-6xl font-InterSans font-bold '}>
+                    <h1 className={'text-2xl md:text-4xl lg:text-6xl font-InterSans font-bold '}>
                         {props.title}
                     </h1>
 
@@ -32,9 +52,9 @@ const ArdeniteEntry = (props: IProps) => {
                     </div>
 
 
-                  <div>
-                      {props.children}
-                  </div>
+                    <div>
+                        {props.children}
+                    </div>
                     <div className={'w-full flex flex-col md:items-start items-center'}>
                         <button className={'bg-white w-2/3 rounded-full text-black text-2xl font-InterSans p-4'}>
                             <div className={'flex flex-row  justify-center opacity-70 '}>
@@ -48,10 +68,11 @@ const ArdeniteEntry = (props: IProps) => {
                 </div>
 
             </div>
-            <div className={'bg-red-100  w-full  min-h-[calc(100vh/2)] md:min-h-fit relative'}>
+            <div className={' transition-all delay-150 duration-1000  w-full  min-h-[calc(100vh/2)] md:min-h-fit relative' + (entryInView.inView ? " opacity-100 translate-x-0 translate-y-0 " : " opacity-0 " + animationDir)}>
 
 
                 <Image src={props.image} layout="fill"
+                       priority={props.priority}
                        className="object-center object-cover pointer-events-none"
                        objectFit="cover">
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,9 @@ import {FloatingNavStatus} from "../components/FloatingNav";
 import ArdeniteOutlineButton from "../components/ArdeniteOutlineButton";
 import ContentContainer from "../components/ContentContainer";
 import ArdeniteHeading from "../components/ArdeniteHeading";
-import Image from "next/image";
 import Backstabber from '../public/static/images/backstabber.png';
-import {ArrowUpRight} from "react-feather";
-import ArdeniteEntry from "../components/ArdeniteEntry";
+import Flowers from '../public/static/images/flowersdontcare.png';
+import ArdeniteEntry, {EntryPosition} from "../components/ArdeniteEntry";
 
 const Home: NextPage = () => {
 
@@ -29,27 +28,43 @@ const Home: NextPage = () => {
                     <div className={"w-screen  flex flex-col items-center h-full absolute z-10 "}>
 
                         <ContentContainer className={"mt-24 h-full  flex flex-col-reverse   w-full"}>
-
-                            <div className={'flex flex-row  items-center w-full h-1/3  justify-center h-full'}>
-                                <ArdeniteHeading></ArdeniteHeading>
+                            <div className={'  flex space-y-4 pb-4 md:space-y-0  flex-col  md:flex-row items-center w-full  md:w-full lg:w-full  md:space-x-5 justify-center lg:space-x-7 pt-8  md:pt-0'}>
+                                <ArdeniteOutlineButton link={"mailto:office@ardenite.net?subject=Order"} title={"LISTEN NOW"} className={"lightBoxPink text-pink bg-brand-black "}/>
+                                <ArdeniteOutlineButton link={"mailto:office@ardenite.net?subject=Order"} title={"WATCH NOW"} className={"lightBoxPink text-pink bg-brand-black "}/>
+                                <ArdeniteOutlineButton link={"mailto:office@ardenite.net?subject=Order"} title={"BUY NOW"} className={"lightBoxPink text-pink bg-brand-black "}/>
                             </div>
+                            <div className={'flex flex-row  items-end w-full  justify-center h-full'}>
+                                <div className={"text-2xl  text-center md:text-2xl leading-6 text-white drop-shadow-xs font-bold font-InterSans  "}>
+                                    CYBER POP ROCK
+                                    <br/>
+                                    BAND FROM VIENNA
+                                </div>
+
+                            </div>
+
                         </ContentContainer>
                     </div>
                 </CrossFade>
-                <ContentContainer className={"h-full flex space-y-8 md:space-y-0  flex-col  md:flex-row items-center w-full  md:w-full lg:w-full  md:space-x-5 justify-center lg:space-x-7 pt-8  md:pt-0 md:-translate-y-[36px]"}>
 
-                    <ArdeniteOutlineButton link={"mailto:office@ardenite.net?subject=Order"} title={"LISTEN NOW"} className={"lightBoxPink text-pink bg-brand-black "}/>
-                    <ArdeniteOutlineButton link={"mailto:office@ardenite.net?subject=Order"} title={"WATCH NOW"} className={"lightBoxPink text-pink bg-brand-black "}/>
-                    <ArdeniteOutlineButton link={"mailto:office@ardenite.net?subject=Order"} title={"BUY NOW"} className={"lightBoxPink text-pink bg-brand-black "}/>
-                </ContentContainer>
 
-                <ArdeniteEntry subtitle={'An explosion of emotions'} image={Backstabber} title={'Backstabber'} direction={''}>
+                <ArdeniteEntry priority={true} subtitle={'We enjoy surprising you'} image={Flowers} title={'Flowers Don´t Care'} animation={EntryPosition.left} className={'bg-teal-900'}>
+                    In that sense “Flowers Don&apos;t Care” may seem like --- and probably is --- your new favourite summer party song! However this is just the bitter sweet package (which hopefully compels you to dance!) of its deeper meaning about life,
+                    death
+                    and dealing with the loss of a close soul.
+                    <br/>
+                    Bittersweet, addictive and liberating all at once! What more could one ask for?
+                </ArdeniteEntry>
+
+
+                <ArdeniteEntry priority={false} subtitle={'An explosion of emotions'} image={Backstabber} className={'bg-[#423058]'} title={'Backstabber'} animation={EntryPosition.right}>
                     Backstabber emerged in an explosion of emotions, caused by the betrayal of a very close person.
                     <br/>
                     As an anthem to the broken hearted, the dominant emotions of the single are rage and anger! <br/>
                     These are noticeable in the chorus and the ironically intonated rap parts. <br/>
                     The protagonist of the song however also struggles with feelings of helplessness & sorrow, while processing the void of an ending relationship.
                 </ArdeniteEntry>
+
+
             </div>
 
 
